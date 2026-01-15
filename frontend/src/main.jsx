be3ef6cb454821ff3d47e2fc726756d0ee3c20e0
Sadat <muhaiminulsadat@gmail.com>
2026-01-15 20:55:44 +0600

    feat: add axios and react-query for API handling, implement user synchronization and authentication hooks

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App.jsx";
 import {ClerkProvider} from "@clerk/clerk-react";
 import {BrowserRouter} from "react-router";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -16,7 +20,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
